6c1b33cc710979bf353343afbc391af0d732c1d4
Richard Wong <ricwtk@gmail.com>
2026-01-20 20:06:17 +0800

    add display for references

---
+++ src/components/course/CourseReferences.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { type Reference } from '@/lib/course';
+import ContentCard from '@/components/contentcard/ContentCard.vue';
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableCell
+} from '@/components/ui/table';
+
+const props = defineProps<{ references: Reference[] }>();
+
+const mainReferences = computed(() => props.references.filter(reference => reference.label === 'main'));
+const additionalReferences = computed(() => props.references.filter(reference => reference.label === 'additional'));
+</script>
+
+<template>
+  <ContentCard editable>
+    <template #title>
+      Main Reference
+    </template>
+    <template #body>
+      <Table>
+        <TableBody>
+          <TableRow v-for="(reference, index) in mainReferences" :key="index">
+            <TableCell>{{ reference.description }}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </template>
+  </ContentCard>
+
+  <ContentCard editable>
+    <template #title>
+      Additional References
+    </template>
+    <template #body>
+      <Table>
+        <TableBody>
+          <TableRow v-for="(reference, index) in additionalReferences" :key="index">
+            <TableCell>{{ reference.description }}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </template>
+  </ContentCard>
+</template>
