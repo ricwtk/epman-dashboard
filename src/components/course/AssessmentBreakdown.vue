9ab59da301faf8c6e20270cea1ce2874e0e1889d
Richard Wong <ricwtk@gmail.com>
2026-01-20 12:00:01 +0800

    add assessment breakdown

--- src/components/course/AssessmentBreakdown.vue
+++ src/components/course/AssessmentBreakdown.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import { type Assessment } from '@/lib/course';
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell
+} from '@/components/ui/table';
+import { CheckIcon, CornerDownRightIcon } from 'lucide-vue-next';
 
 const props = defineProps<{
   assessments: Assessment[] | undefined;
@@ -8,15 +17,47 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div v-for="(assessment, index) in assessments" :key="index">
-    <h4>{{ assessment.description }}</h4>
-    <div v-if="assessment.breakdown.length > 0">
-      <div v-for="(item, itemIndex) in assessment.breakdown" :key="itemIndex">
-        {{ item.description }}: {{ item.weightage }}%
-      </div>
-    </div>
-    <div v-else>
-      {{ assessment.weightage }}%
-    </div>
-  </div>
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead class="font-bold">Method</TableHead>
+        <TableHead class="font-bold text-center">Weightage</TableHead>
+        <TableHead class="font-bold text-center" v-for="index in coCount" :key="index">CO{{ index }}</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <template v-for="(assessment, assessmentIndex) in assessments" :key="assessmentIndex">
+
+        <TableRow>
+          <TableCell>{{ assessment.description }}</TableCell>
+          <TableCell class="text-center">{{ assessment.breakdown.length > 0 ? '' : `${assessment.weightage} %` }}</TableCell>
+          <TableCell class="text-center"
+            v-for="coIndex in coCount"
+            :key="coIndex"
+          >
+            <template v-if="!(assessment.breakdown.length > 0)">
+              <CheckIcon class="inline-block" :size="16" v-if="assessment.cos.includes(coIndex)"></CheckIcon>
+            </template>
+          </TableCell>
+        </TableRow>
+
+        <template v-if="assessment.breakdown.length > 0">
+          <TableRow v-for="(item, itemIndex) in assessment.breakdown" :key="itemIndex">
+            <TableCell class="flex flex-row items-center">
+              <CornerDownRightIcon class="inline-block" :size="16" />
+              <div class="inline-block ml-1">{{ item.description }}</div>
+            </TableCell>
+            <TableCell class="text-center">{{ item.weightage }}%</TableCell>
+            <TableCell class="text-center"
+              v-for="coIndex in coCount"
+              :key="coIndex"
+            >
+              <CheckIcon class="inline-block" :size="16" v-if="item.co == coIndex"></CheckIcon>
+            </TableCell>
+          </TableRow>
+        </template>
+
+      </template>
+    </TableBody>
+  </Table>
 </template>
--- src/components/course/CourseAssessments.vue
+++ src/components/course/CourseAssessments.vue
@@ -5,6 +5,9 @@ import { type Assessment } from '@/lib/course'
 import AssessmentMainTable from '@/components/course/AssessmentMainTable.vue';
 import AssessmentBreakdown from '@/components/course/AssessmentBreakdown.vue';
 import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { EyeIcon, EyeOffIcon } from 'lucide-vue-next';
+import { ref } from 'vue';
 
 const props = defineProps<{
   assessments: Assessment[] | undefined;
@@ -14,6 +17,12 @@ const props = defineProps<{
 const anybreakdown = computed(() => {
   return props.assessments?.some(assessment => assessment.breakdown.length > 0);
 });
+
+const showBreakdown = ref(false);
+
+const toggleBreakdown = () => {
+  showBreakdown.value = !showBreakdown.value;
+};
 </script>
 
 <template>
@@ -24,9 +33,19 @@ const anybreakdown = computed(() => {
     <template #body>
       <div v-if="assessments">
         <AssessmentMainTable :assessments="assessments" :coCount="coCount"/>
-        <div v-if="anybreakdown">
-          <Badge class="mt-2">Breakdown</Badge>
-          <AssessmentBreakdown :assessments="assessments" :coCount="coCount"/>
+        <div v-if="anybreakdown" class="mt-1">
+          <div class="flex flex-row items-center">
+            <Badge>Breakdown</Badge>
+            <Button variant="ghost" @click="toggleBreakdown">
+              <EyeIcon v-if="!showBreakdown" />
+              <EyeOffIcon v-else />
+            </Button>
+          </div>
+          <AssessmentBreakdown
+            :assessments="assessments"
+            :coCount="coCount"
+            v-show="showBreakdown"
+          />
         </div>
       </div>
     </template>
