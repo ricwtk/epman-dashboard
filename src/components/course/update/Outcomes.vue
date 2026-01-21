e0921d0370d0ff645a9fafb2b0b07f74364f0c65
Richard Wong <ricwtk@gmail.com>
2026-01-21 23:26:39 +0800

    update font; update outcomes update ui to grid mapping

--- src/components/course/update/Outcomes.vue
+++ src/components/course/update/Outcomes.vue
@@ -7,6 +7,8 @@ import MultiSelect from '@/components/multiselectwithfilter/MultiSelect.vue'
 import { Separator } from '@/components/ui/separator'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
+import { Table, TableBody, TableHead, TableHeader, TableRow, TableCell } from '@/components/ui/table'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { ChevronUpIcon, ChevronDownIcon, MinusIcon, PlusIcon } from 'lucide-vue-next'
 
 const colist = ref<Co[]>([{
@@ -28,11 +30,238 @@ const colist = ref<Co[]>([{
   eas: [3],
   sdg: false
 }])
+
+const bloomtaxOptions = [
+  { domain: "Cognitive", levels: [
+    'Remember', 'Understand', 'Apply', 'Analyze', 'Synthesize', 'Evaluate'
+  ]},
+  { domain: "Affective", levels: [
+    'Receiving', 'Responding', 'Valuing', 'Organization', 'Characterizing by a Value'
+  ]},
+  { domain: "Psychomotor", levels: [
+    'Imitation', 'Manipulation', 'Precision', 'Articulation', 'Naturalisation'
+  ]}
+]
+
+const poOptions = [
+  "Engineering Knowledge",
+  "Problem Analysis",
+  "Design/Development of Solutions",
+  "Investigation",
+  "Tool Usage",
+  "The Engineer and the World",
+  "Ethics",
+  "Individual and Collaborative Team Work",
+  "Communication",
+  "Project Management and Finance",
+  "Lifelong Learning"
+]
+
+const wkOptions = [
+  "Natural and Social Sciences",
+  "Mathematics and Computer Science",
+  "Engineering Fundamentals",
+  "Engineering Specialist Knowledge",
+  "Engineering Design and Operations",
+  "Engineering Practice",
+  "Engineers in Society",
+  "Research Skills",
+  "Professional and Ethical Responsibility"
+]
+
+const wpOptions = [
+  "Depth of Knowledge Required",
+  "Range of Conflicting Requirements",
+  "Depth of Analysis Required",
+  "Familiarity of Issues",
+  "Extent of Applicable Codes",
+  "Extent of Stakeholder Involvement and Conflicting Requirements",
+  "Interdependence"
+]
+
+const eaOptions = [
+  "Range of Resources",
+  "Level of Interactions",
+  "Innovation",
+  "Consequences to Society and the Environment",
+  "Familiarity"
+]
+
 </script>
 
 <template>
   <div class="flex flex-col gap-2">
-    <div
+    <div class="font-semibold">CO Definition</div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead></TableHead>
+          <TableHead class="">#</TableHead>
+          <TableHead class="">Description</TableHead>
+          <TableHead class="text-center">Bloom Taxonomy</TableHead>
+          <TableHead class="text-center">SDG</TableHead>
+          <TableHead></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="(co, coIndex) in colist" :key="coIndex">
+          <TableCell class="w-14 align-top text-center">
+            <Button variant="destructive" @click="console.log(`Deleting reference ${refIndex}`)"><MinusIcon /></Button>
+          </TableCell>
+          <TableCell class="align-top">{{ coIndex + 1 }}</TableCell>
+          <TableCell class="align-top">
+            <Textarea :id="'desc' + coIndex" :value="co.description" />
+          </TableCell>
+          <TableCell class="w-0 align-top">
+            <Select>
+              <SelectTrigger class="w-40">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup v-for="(domain, index) in bloomtaxOptions" :key="index">
+                  <SelectLabel>{{ domain.domain }}</SelectLabel>
+                  <SelectItem v-for="(level, levelIndex) in domain.levels" :key="levelIndex" :value="`${domain.domain[0]}${levelIndex}`">
+                    {{ `${domain.domain[0]}${levelIndex} ${level}` }}
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Select v-model="co.bloomtax" :options="bloomtaxOptions" />
+          </TableCell>
+          <TableCell class="w-0 align-top text-center">
+            <Checkbox :default-value="false" :id="`co${coIndex + 1}sdg`"/>
+          </TableCell>
+          <TableCell class="w-14 align-top text-center">
+            <div class="flex flex-col gap-1">
+              <Button
+                variant="secondary"
+                :disabled="coIndex === 0"
+                @click="console.log(`Move up ${coIndex}`)"
+              ><ChevronUpIcon /></Button>
+              <Button
+                variant="secondary"
+                :disabled="coIndex === colist.length - 1"
+                @click="console.log(`Move down ${coIndex}`)"
+              ><ChevronDownIcon /></Button>
+            </div>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+
+    <Button variant="default"><PlusIcon /></Button>
+
+    <div class="font-semibold">CO to Programme Outcome (PO) Mapping</div>
+
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="align-bottom text-center w-0">CO</TableHead>
+          <TableHead class="align-bottom text-center"
+            v-for="(po, poIndex) in poOptions"
+            :key="poIndex"
+          >
+            <span class="vertical-text">
+              {{ `PO${poIndex + 1} ${po}` }}
+            </span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="(co, coIndex) in colist" :key="coIndex" class="text-center">
+          <TableCell class="w-0">CO{{ coIndex + 1 }}</TableCell>
+          <TableCell v-for="(po, poIndex) in poOptions" :key="poIndex">
+            <Checkbox :default-value="false" :id="`co${coIndex + 1}po${poIndex + 1}`"/>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+
+    <div class="font-semibold">CO to Knowledge Profile (WK) Mapping</div>
+
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="align-bottom text-center w-0">CO</TableHead>
+          <TableHead class="align-bottom text-center w-0">PO</TableHead>
+          <TableHead class="align-bottom text-center"
+            v-for="(wk, wkIndex) in wkOptions"
+            :key="wkIndex"
+          >
+            <span class="vertical-text">
+              {{ `WK${wkIndex + 1} ${wk}` }}
+            </span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="(co, coIndex) in colist" :key="coIndex" class="text-center">
+          <TableCell class="w-0">CO{{ coIndex + 1 }}</TableCell>
+          <TableCell class="w-0">{{ co.pos.map(pos => `PO${pos + 1}`).join(', ') }}</TableCell>
+          <TableCell v-for="(wk, wkIndex) in wkOptions" :key="wkIndex">
+            <Checkbox :default-value="false" :id="`co${coIndex + 1}wk${wkIndex + 1}`"/>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+
+    <div class="font-semibold">CO to Complex Engineering Problem (WP) Mapping</div>
+
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="align-bottom text-center w-0">CO</TableHead>
+          <TableHead class="align-bottom text-center w-0">PO</TableHead>
+          <TableHead class="align-bottom text-center"
+            v-for="(wp, wpIndex) in wpOptions"
+            :key="wpIndex"
+          >
+            <span class="vertical-text">
+              {{ `WP${wpIndex + 1} ${wp}` }}
+            </span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="(co, coIndex) in colist" :key="coIndex" class="text-center">
+          <TableCell class="w-0">CO{{ coIndex + 1 }}</TableCell>
+          <TableCell class="w-0">{{ co.pos.map(pos => `PO${pos + 1}`).join(', ') }}</TableCell>
+          <TableCell v-for="(wp, wpIndex) in wpOptions" :key="wpIndex">
+            <Checkbox :default-value="false" :id="`co${coIndex + 1}wp${wpIndex + 1}`"/>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+
+    <div class="font-semibold">CO to Complex Engineering Activities (EA) Mapping</div>
+
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="align-bottom text-center w-0">CO</TableHead>
+          <TableHead class="align-bottom text-center w-0">PO</TableHead>
+          <TableHead class="align-bottom text-center"
+            v-for="(ea, eaIndex) in eaOptions"
+            :key="eaIndex"
+          >
+            <span class="vertical-text">
+              {{ `EA${eaIndex + 1} ${ea}` }}
+            </span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="(co, coIndex) in colist" :key="coIndex" class="text-center">
+          <TableCell class="w-0">CO{{ coIndex + 1 }}</TableCell>
+          <TableCell class="w-0">{{ co.pos.map(pos => `PO${pos + 1}`).join(', ') }}</TableCell>
+          <TableCell v-for="(ea, eaIndex) in eaOptions" :key="eaIndex">
+            <Checkbox :default-value="false" :id="`co${coIndex + 1}ea${eaIndex + 1}`"/>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+
+    <!-- <div
       v-for="(co, coIndex) in colist"
       :key="coIndex"
       class="flex flex-col gap-3"
@@ -101,6 +330,16 @@ const colist = ref<Co[]>([{
       <Separator class="my-5" v-if="coIndex < colist.length - 1"/>
     </div>
 
-    <Button variant="default"><PlusIcon /></Button>
+    <Button variant="default"><PlusIcon /></Button> -->
   </div>
 </template>
+
+<style scoped>
+.vertical-text {
+  writing-mode: sideways-lr;
+  text-orientation: mixed;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
