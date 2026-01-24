159029463839ba68a9f12a129603aa3154668570
Richard Wong <ricwtk@gmail.com>
2026-01-24 21:20:24 +0800

    update programme display ui

--- src/components/programme/PoMapping.vue
+++ src/components/programme/PoMapping.vue
@@ -30,13 +30,17 @@ defineEmits(['update:editing']);
           <TableRow>
             <TableHead class="text-center" rowspan="2">PO</TableHead>
             <TableHead class="text-center" rowspan="2">PEO</TableHead>
+            <TableHead class="w-0 bg-border"></TableHead>
             <TableHead class="text-center" colspan="3">Exam Based</TableHead>
+            <TableHead class="w-0 bg-border"></TableHead>
             <TableHead class="text-center" colspan="3">Project Based</TableHead>
           </TableRow>
           <TableRow>
+            <TableHead class="w-0 bg-border"></TableHead>
             <TableHead class="text-center">WK</TableHead>
             <TableHead class="text-center">WP</TableHead>
             <TableHead class="text-center">EA</TableHead>
+            <TableHead class="w-0 bg-border"></TableHead>
             <TableHead class="text-center">WK</TableHead>
             <TableHead class="text-center">WP</TableHead>
             <TableHead class="text-center">EA</TableHead>
@@ -48,6 +52,7 @@ defineEmits(['update:editing']);
             <TableCell class="text-center">
               <BadgeList :items="[`PEO${po.mapping.peo}`]" />
             </TableCell>
+            <TableCell class="w-4 bg-border"></TableCell>
             <TableCell class="text-center">
               <BadgeList :items="po.mapping.examBased.wk.map((wk) => `WK${wk}`)" />
             </TableCell>
@@ -57,6 +62,7 @@ defineEmits(['update:editing']);
             <TableCell class="text-center">
               <BadgeList :items="po.mapping.examBased.ea.map((ea) => `EA${ea}`)" />
             </TableCell>
+            <TableCell class="w-4 bg-border"></TableCell>
             <TableCell class="text-center">
               <BadgeList :items="po.mapping.projectBased.wk.map((wk) => `WK${wk}`)" />
             </TableCell>
