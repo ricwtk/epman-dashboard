37c680687bff9a119f7f207bd67ddcf95c9b1f52
Richard Wong <ricwtk@gmail.com>
2026-02-12 16:41:55 +0800

    add functions to prepare for email login

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  // Add all other VITE_ prefixed variables you use
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+  readonly VITE_CLOUDRUN_URL: string;
+  readonly VITE_BASE_URL: string;
+  readonly VITE_LANDING_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
---
+++ src/composables/useAuth.ts
@@ -0,0 +1,49 @@
+// src/composables/useAuth.js
+import { ref } from "vue";
+import { auth } from "@/lib/firebase";
+import {
+  sendSignInLinkToEmail,
+  signInWithEmailLink,
+  onAuthStateChanged,
+  isSignInWithEmailLink
+} from "firebase/auth";
+
+export const user = ref(null);
+
+// onAuthStateChanged(auth, (u) => {
+//   if (u) {
+//     user.value = u
+//   }
+// });
+
+// Step 1: send sign-in email link
+export async function sendLoginLink(email: string): Promise<void> {
+  const actionCodeSettings = {
+    url: window.location.origin + import.meta.env.VITE_BASE_URL, // must match Firebase config
+    handleCodeInApp: true,
+  };
+  return sendSignInLinkToEmail(auth, email, actionCodeSettings)
+    .then(() => {
+      window.localStorage.setItem("epman:emailForSignIn", email);
+    })
+}
+
+// Step 2: when user clicks link and opens your site
+export async function completeSignIn(): Promise<unknown> {
+  if (isSignInWithEmailLink(auth, window.location.href)) {
+    let email = window.localStorage.getItem("epman:emailForSignIn");
+    if (!email) {
+      email = window.prompt("Please confirm your email for sign-in");
+    }
+    console.log(window.location.href)
+    return signInWithEmailLink(auth, email!, window.location.href)
+    .then((result) => {
+      window.localStorage.removeItem("emailForSignIn");
+      return result.user
+    })
+  } else {
+    return new Promise((resolve, reject) => {
+      resolve(null)
+    })
+  }
+}
