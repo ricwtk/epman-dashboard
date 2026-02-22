13d51e914478a9a2aaa15da0744fa31c7527649b
Richard Wong <ricwtk@gmail.com>
2026-02-22 19:01:25 +0800

    update for deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import SchoolListView from '../views/SchoolListView.vue'
 import LoginView from '@/views/LoginView.vue'
 import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [{
     path: '/',
     name: 'login',
