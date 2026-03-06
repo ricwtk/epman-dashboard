b4f61de487741cc480013642f93fa16cb110818a
Richard Wong <ricwtk@gmail.com>
2026-03-06 23:46:14 +0800

    remove viewing course and editing course store definition

--- src/composables/course.ts
+++
@@ -1,8 +0,0 @@
-import { storeToRefs } from 'pinia';
-import { useEditingCourseStore } from '@/stores/editingcourse';
-
-export function getEditingCourseAndStore() {
-  const editingCourseStore = useEditingCourseStore();
-  const { course } = storeToRefs(editingCourseStore);
-  return { course, editingCourseStore };
-}
