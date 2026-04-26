abaac490f041852af23710f9a85ecd62b3737869
Richard Wong <ricwtk@gmail.com>
2026-04-26 23:26:29 +0800

    clean up removed component: CourseReferences

--- src/components/courseimport/Overview.vue
+++ src/components/courseimport/Overview.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import type { Course } from '@/types/course';
 import CourseSummary from '../course/CourseSummary.vue';
 import CourseOutcomes from '../course/CourseOutcomes.vue';
 import CourseAssessments from '../course/CourseAssessments.vue';
 import CEPCEAImplementation from '../course/CEPCEAImplementation.vue';
 import CoursePlan from '../course/CoursePlan.vue';
-import CourseReferences from '../course/CourseReferences.vue';
 import ReferenceList from '../course/ReferenceList.vue';
 
 defineProps<{
@@ -44,9 +42,4 @@ defineProps<{
     title="Additional References"
     referenceLabel="additional"
   />
-  <!-- <CourseReferences
-    :references="course.references || []"
-    :editing="false"
-    :editable="false"
-  ></CourseReferences> -->
 </template>
